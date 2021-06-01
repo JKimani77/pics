cfc37378bde1b1142ab1c9d3f9d2eb41caebd11d
JKimani77 <jkkimani77@gmail.com>
2021-06-01 13:50:10 +0300

    created urls file and homepage view

--- apppics/views.py
+++ apppics/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http  import Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect,HttpResponse
+#from .forms import LoginForm,ProjectForm,ProfileForm,RatingForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+
+# Create your views he
+from .models import *
+#from rest_framework.response import Response
+#from rest_framework.views import APIView
+#from .serializers import ProjectSerializer,ProfileSerialiser
+#from rest_framework.views import APIView
+# need
 
 # Create your views here.
+def home(request):
+    #projects = Project.objects.all()
+
+    return render(request, 'home.html')
+
